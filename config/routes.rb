14ed212b993bbd3e0bfbd01c12ef754f9da588fa
Jake Oldham <jakemoldham@gmail.com>
2015-03-21 11:45:05 -0600

    fixed devise.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
   Rails.application.routes.draw do
-  
+
+  root to: "events#index"
+    devise_for :users, :controllers => { registrations: 'registrations' }
   get 'users/show'
   
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  
  
   resources :events
 
